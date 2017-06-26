37c7303da596347ad7305915c2dca9cead26f9c9
Juraj Kubelka <juraj.kubelka@icloud.com>
2017-06-26 12:16:55 +0200

    add basic info for user

--- web/index.php
+++ web/index.php
@@ -1,9 +1,19 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
  <head>
-  <title>Prueba de PHP</title>
+  <meta charset="utf-8" />
+  <title>Discord for Pharo</title>
+  <meta name="author" content="Juraj Kubelka" />
+  <meta name="description" content="Download a serialized data from a Discord message to Pharo.">
+  
  </head>
  <body>
- <?php echo '<p>Hi World</p>'; ?>
+
+<p>Hi!</p>
+<p>You have openned this page by clicking on a link in your Discord client. 
+It means that the message includes a serialized data, e.g., objects, stack trace, 
+that can be downloaded and displayed in your Pharo image.</p>
+<p>To do so, copy the following code, paste it to your Pharo Playground (Workspace) and execute it.</p> 
  
  <pre><code>
  Metacello new
@@ -13,5 +23,8 @@
  #DSLoadCommand asClassIfPresent: [ :theClass | theClass loadId: '<?php 
  	echo filter_input( INPUT_GET, 'id', FILTER_SANITIZE_URL ); ?>' ]. 
  </code></pre>
+ 
+ <p>It will download the Discord client available for Pharo, and after authentication, it will download the data.</p>
+ 
  </body>
 </html>
