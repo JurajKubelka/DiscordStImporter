ffe3993841a7ae1b033c52e605886500c437f78b
Juraj Kubelka <juraj.kubelka@icloud.com>
2017-06-27 15:58:39 +0200

    add dependency on DiscordSt client branch

--- web/index.php
+++ web/index.php
@@ -57,7 +57,7 @@
 		<code>
  Metacello new
     baseline: #DiscordSt;
-    repository: 'github://JurajKubelka/DiscordSt:command-extensions/src';
+    repository: 'github://JurajKubelka/DiscordSt:client/src';
     load.
  #DSLoadCommand asClassIfPresent: [ :theClass | theClass loadId: '<?php
 	echo filter_input ( INPUT_GET, 'id', FILTER_SANITIZE_URL );
